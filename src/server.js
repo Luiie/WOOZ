8e7ad2ed700802d6a8d193cec2d88739efd9a7a7
LuiieClone <dbsldkfls@gmail.com>
2023-05-08 14:57:14 +0900

    Env setting(0th)

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+app.use("/public", express.static(__dirname+"/public"));
+
+app.listen(3000);
