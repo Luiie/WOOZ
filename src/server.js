31d7ce167c7cd3a067f9b6530f9ad31f44e305b7
LuiieClone <dbsldkfls@gmail.com>
2023-05-10 10:34:22 +0900

    (ch.1)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,39 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname+"/views");
+app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-app.use("/public", express.static(__dirname+"/public"));
 
-app.listen(3000);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous"
+    console.log("Connected to Browser");
+    // socket.send("Hello from the Server");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "nickname":
+                socket["nickname"] = message.payload;
+            case "new_msg":
+                sockets.forEach((aSocket) => {
+                    aSocket.send(`${socket.nickname}: ${message.payload}`);
+                });
+        }
+    });
+});
+
+const handleListen = () => console.log(`Listening on...`)
+server.listen(3000, handleListen);
